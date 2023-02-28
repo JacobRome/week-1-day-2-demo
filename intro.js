35e0516efd8cbe166c9ddd3d57b815cf8d912ead
Jacob rome <ejrome17@gmail.com>
2023-02-28 15:42:24 -0700

    added demo file

---
+++ intro.js
@@ -0,0 +1,28 @@
+
+
+// console.log("hello,world"); 
+
+// const Name = "Jacob";
+
+// console.log(Name); 
+
+ //let age = 18;
+
+ //const isCool = true; 
+
+// console.log(Name,age,isCool)
+
+// console.log("hello, my name is"+ Name "i am" + age "years old")
+// console.log("hello my name is" {Name} i am {age} years old")
+
+let x = 4
+let y = 10
+let z = x + y + z 
+console.log(x)
+console.log(y)
+console.log(z)
+
+console.log(sum) 
+
+
+
